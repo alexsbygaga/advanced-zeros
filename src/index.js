45d071d5d4a8a545374a2a0e31efdb5546bc2208
alexsbygaga <alexsbygaga@gmail.com>
2018-03-01 11:12:07 +0300

    src/index.js first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    var baseNum = 0;
+    var res = 0;
+    if(base % 2 != 0){
+      baseNum = base;
+    }else{
+        while(base % 2 == 0 ){
+            base = base/2;
+            baseNum = base;
+        }
+    }
+    if (baseNum==1){
+        res=Math.floor(number/5);
+    }else{
+    while(number){
+        number = (Math.floor(number/baseNum));
+        res+=number;
+    }}
+    return res;
+} 
